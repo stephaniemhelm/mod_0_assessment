04191c1833e42d5100cb2cf2ba6315b471359a33
Stephanie Helm <stephaniemhelm@gmail.com>
2021-06-11 12:01:20 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,13 @@
+movie = "Harry Potter"
+is_entertaining = true
+movie = "Twilight"
+
+
+num_4 = 10
+num_6 = 55
+product = num_4 * num_6
+
+favorite_foods = ["watermellon", "peaches", "mashed potatoes"]
+favorite_foods.delete_at(0)
+
+puts movie.length
