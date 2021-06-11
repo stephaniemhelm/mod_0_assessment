92f2507d252a1be5477769d43652ff07062f0792
Stephanie Helm <stephaniemhelm@gmail.com>
2021-06-11 12:21:03 -0600

    Initial commit

---
+++ models/model_a.rb
@@ -0,0 +1,14 @@
+dinosaur = {
+  name: "T-Rex",
+  special_ability: "most ferocious preditor to walk the earth",
+  height_in_feet: 20,
+  weight_in_pounds: 15000,
+  ferocious: true,
+  extinct: true,
+  ate: ["edmontosaurus", "anatosaurus", "triceratops"],
+  lived: {
+    location: "North America",
+    time_period: "late cretaceous period"
+  }
+}
+ puts dinosaur
