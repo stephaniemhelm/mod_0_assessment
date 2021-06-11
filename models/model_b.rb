22ab0097c2d04bbb99192d98a445ade97bce6004
Stephanie Helm <stephaniemhelm@gmail.com>
2021-06-11 12:30:17 -0600

    Initial commit

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+developer = {
+  name: "Stephanie",
+  company: "Turing",
+  age: 40,
+  years_coding: 1,
+  new_developer: true,
+  senior_developer: false,
+  work_locations: ["home", "library", "school"],
+  prospective_employers: ["Target", "Google", "Instagram"]
+}
+
+puts developer
